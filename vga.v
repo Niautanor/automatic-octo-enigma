04ec427233ee163d1246cee4e682d58f2afd9168
Luna Gräfje <Luna@baguette.management>
2020-07-18 21:58:08 +0200

    Add VGA Data enable signal
    Make &s more correct
    
    Supposedly, color output drivers should be disabled (high z state)
    during the blanking period. I'm not quite sure if that's really required
    but I'm doing it just to be safe

--- vga.v
+++ vga.v
@@ -1,4 +1,4 @@
-module vga(input clk, output hsync, output vsync);
+module vga(input clk, output hsync, output vsync, output de);
 
 reg [1:0] pixelcnt;
 wire pixelclk = (pixelcnt == 0);
@@ -10,14 +10,16 @@ always @(posedge clk) if (pixelclk) begin
     if (x == 799) x <= 0;
 end
 
-assign hsync = x >= (640 + 16) && x < (640 + 16 + 96);
+assign hsync = x >= (640 + 16) & x < (640 + 16 + 96);
 
 reg [9:0] y = 523;
-always @(posedge clk) if (pixelclk && (x == 799)) begin
+always @(posedge clk) if (pixelclk & (x == 799)) begin
     y <= y + 1;
     if (y == 523) y <= 0;
 end
 
-assign vsync = y >= (480 + 11) && y < (480 + 11 + 2);
+assign vsync = y >= (480 + 11) & y < (480 + 11 + 2);
+
+assign de = x < 640 & y < 480;
 
 endmodule
