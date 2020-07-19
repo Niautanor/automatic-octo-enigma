8e7bd15d76edc3a9ebcbd2e3563b3b9db9a84c9a
Luna Gräfje <Luna@baguette.management>
2020-07-19 14:26:26 +0200

    Sanitize VGA code
    First of all: don't start counting at 799 so that you can transition to
    0 at the first clock cycle. That's dumb.
    
    Second: introduce {x,y}_next wires to be able to synchronously fetch
    pixel data from ram later

--- vga.v
+++ vga.v
@@ -1,22 +1,18 @@
 module vga(input clk, output hsync, output vsync, output [1:0] r, output [1:0] g, output [1:0] b);
 
 reg [1:0] pixelcnt;
-wire pixelclk = (pixelcnt == 0);
+wire pixelclk = (pixelcnt == 3);
 always @(posedge clk) pixelcnt <= pixelcnt + 1;
 
-reg [9:0] x = 799;
-always @(posedge clk) if (pixelclk) begin
-    x <= x + 1;
-    if (x == 799) x <= 0;
-end
+reg [9:0] x = 0;
+wire [9:0] x_next = (x == 799) ? 0 : x + 1;
+always @(posedge clk) if (pixelclk) x <= x_next;
 
 assign hsync = x >= (640 + 16) & x < (640 + 16 + 96);
 
-reg [9:0] y = 523;
-always @(posedge clk) if (pixelclk & (x == 799)) begin
-    y <= y + 1;
-    if (y == 523) y <= 0;
-end
+reg [9:0] y = 0;
+wire [9:0] y_next = (x == 799) ? (y == 523) ? 0 : y + 1 : y;
+always @(posedge clk) if (pixelclk) y <= y_next;
 
 assign vsync = y >= (480 + 11) & y < (480 + 11 + 2);
 
