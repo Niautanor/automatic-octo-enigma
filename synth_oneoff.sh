9ecd80ee00e5dbb9ad4134414503ce2f7e189005
Luna Gräfje <Luna@baguette.management>
2020-07-19 18:22:43 +0200

    Prettier synth_oneoff

--- synth_oneoff.sh
+++ synth_oneoff.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-yosys -p "synth_ice40 -blif $1.blif" $1.v && arachne-pnr -d 8k -P tq144:4k -p $1.pcf $1.blif -o $1.txt && icepack $1.txt $1.bin
+yosys -p "synth_ice40 -blif $1.blif" $1.v && \
+arachne-pnr -d 8k -P tq144:4k -p $1.pcf $1.blif -o $1.txt && \
+icepack $1.txt $1.bin
