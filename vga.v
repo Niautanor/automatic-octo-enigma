200ef3c5e2dfbd79b4af76250b4dcb88a2ac421e
Luna Gräfje <Luna@baguette.management>
2020-07-19 14:09:14 +0200

    Don't set outputs to high impedance during blanking
    Apparently this makes results worse and not better *shrug*

--- vga.v
+++ vga.v
@@ -1,5 +1,3 @@
-`include "output_pin_with_enable.v"
-
 module vga(input clk, output hsync, output vsync, output [1:0] r, output [1:0] g, output [1:0] b);
 
 reg [1:0] pixelcnt;
@@ -22,17 +20,8 @@ end
 
 assign vsync = y >= (480 + 11) & y < (480 + 11 + 2);
 
-wire de = x < 640 & y < 480;
-
-wire [1:0] r_data = x[3:2];
-wire [1:0] g_data = x[3:2];
-wire [1:0] b_data = x[3:2];
-
-output_pin_with_enable r0 (.data(r_data[0]), .enable(de), .pin(r[0]));
-output_pin_with_enable r1 (.data(r_data[1]), .enable(de), .pin(r[1]));
-output_pin_with_enable g0 (.data(g_data[0]), .enable(de), .pin(g[0]));
-output_pin_with_enable g1 (.data(g_data[1]), .enable(de), .pin(g[1]));
-output_pin_with_enable b0 (.data(b_data[0]), .enable(de), .pin(b[0]));
-output_pin_with_enable b1 (.data(b_data[1]), .enable(de), .pin(b[1]));
+assign r = (x < 640) ? x[3:2] : 0;
+assign g = (x < 640) ? x[5:4] : 0;
+assign b = (x < 640) ? x[7:6] : 0;
 
 endmodule
