405725de9cdeff0220caf9c3f30e706e0a50a460
Luna Gräfje <Luna@baguette.management>
2020-07-19 17:03:50 +0200

    Try to read an image from a memory
    This fails right now because yosys thinks the memory read is clocked
    with 1'x (???)

--- vga.v
+++ vga.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module vga(input clk, output hsync, output vsync, output [1:0] r, output [1:0] g, output [1:0] b);
 
 reg [1:0] pixelcnt;
@@ -16,8 +18,16 @@ always @(posedge clk) if (pixelclk) y <= y_next;
 
 assign vsync = y >= (480 + 11) & y < (480 + 11 + 2);
 
-assign r = (x < 640) ? x[3:2] : 0;
-assign g = (x < 640) ? x[5:4] : 0;
-assign b = (x < 640) ? x[7:6] : 0;
+reg [7:0] memory[0:127][0:127];
+initial $readmemh("data/image.hex", memory);
+
+reg [7:0] pixel = 0;
+always @(posedge clk) begin
+    pixel <= memory[x_next][y_next];
+end
+
+assign r = pixel[5:4];
+assign g = pixel[3:2];
+assign b = pixel[1:0];
 
 endmodule
