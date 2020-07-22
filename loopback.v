c4501d7cb7166c2ee3a93eb3b5468434e1d4b3e2
Luna Gräfje <Luna@baguette.management>
2020-07-22 14:18:02 +0200

    Add a small (1 element) fifo to loopback

--- loopback.v
+++ loopback.v
@@ -3,18 +3,30 @@
 
 `default_nettype none
 
-module loopback(input clk, input rx, output tx, output reg [3:0] leds);
+module loopback(input clk, input rx, output tx, output [3:0] leds);
 
 localparam BAUD = 9600;
 
 wire rx_ready;
 wire [7:0] rx_data;
+wire tx_ready;
 
+reg [7:0] fifo = 0;
+reg fifo_avail = 0;
 always @(posedge clk) begin
-    if (rx_ready) leds <= rx_data[3:0];
+    if (tx_ready) fifo_avail <= 0;
+    if (rx_ready) begin
+        fifo <= rx_data;
+        fifo_avail <= 1;
+    end
 end
 
+assign leds[0] = fifo_avail;
+assign leds[1] = rx_ready;
+assign leds[2] = tx_ready;
+assign leds[3] = fifo[0];
+
 uart_rx #(.BAUD(BAUD)) rx_ctl (.clk(clk), .data_ready(rx_ready), .data(rx_data), .rx(rx));
-uart_tx #(.BAUD(BAUD)) tx_ctl (.clk(clk), .rst(0), .en(rx_ready), .data_in(rx_data), .rdy(), .tx(tx));
+uart_tx #(.BAUD(BAUD)) tx_ctl (.clk(clk), .rst(0), .en(fifo_avail), .data_in(rx_data), .rdy(tx_ready), .tx(tx));
 
 endmodule
