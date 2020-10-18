581c1ddb423371c3a28b12830f7f843cdb4b21d0
Luna Gräfje <Luna@baguette.management>
2020-10-18 17:41:27 +0200

    Allow passing parameters to yosys in synth_oneoff.sh

--- synth_oneoff.sh
+++ synth_oneoff.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-yosys -p "synth_ice40 -json $1.json" $1.v && \
-nextpnr-ice40 --json $1.json --freq 100 --hx8k --package tq144:4k --pcf $1.pcf --asc $1.txt && \
-icepack $1.txt $1.bin
+TARGET=$1
+shift
+yosys "$@" -p "synth_ice40 -json ${TARGET}.json" ${TARGET}.v && \
+nextpnr-ice40 --json ${TARGET}.json --freq 100 --hx8k --package tq144:4k --pcf ${TARGET}.pcf --asc ${TARGET}.txt && \
+icepack ${TARGET}.txt ${TARGET}.bin
